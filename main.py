f780dc03a37e11f64bb4e2dce90e130b74c31362
YD <zeroistfilm@naver.com>
2022-07-15 18:46:41 +0900

    init

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    'https: // klipwallet.com',
+    'https://klipwallet.com',
     'https://stg.klipwallet.com'
 ]
 
