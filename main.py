63b863a9e5e3fcd73bfff5b3e9c28d560692c2b9
YD <zeroistfilm@naver.com>
2022-07-15 18:26:49 +0900

    init

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from fastapi.responses import FileResponse, JSONResponse
 import requests
 app = FastAPI()
+from pydantic import BaseModel
 
-URL ='https://storage.googleapis.com/urbanbreakxpass2022/json/2000.json'
 
-@app.get('/')
+
+
+
+@app.get('/json/{tokenId}.json')
+async def f(tokenId):
+    resData = {
+        "tokenId": tokenId,
+        "name": f'X PASS #{tokenId}',
+        "description": "Welcome Aboard, URBAN BREAK's X PASS NFT Members!   \n\nURBAN BREAK’s first X PASS NFT membership service will present a whole new experience the members have never encountered before.",
+        "image": "http://34.64.193.146:8000/image",
+        "animation_url": "http://34.64.193.146:8000/video",
+        "attributes": [
+            {
+                "trait_type": "Tier",
+                "value": "2022"
+            }
+        ]
+    }
+
+    return JSONResponse(resData)
+
+
+@app.get('/image')
 async def f():
-    URL = 'https://storage.googleapis.com/urbanbreakxpass2022/json/2000.json'
-    response = requests.get(URL)
-    return response.json()
+    return FileResponse('./assets/xpass.gif')
+
+@app.get('/video')
+async def f():
+    return FileResponse('./assets/xpass.mp4')
 
 
 
